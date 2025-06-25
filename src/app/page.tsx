2082baba12b817502074a865bb82defd16cd32e3
jeanepp <jeanepp99@gmail.com>
2025-06-25 22:13:28 +0200

    correcting time load

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,13 @@
-import dynamic from 'next/dynamic';
 import Accueil from "./Page/Accueil";
 import Perfumes from './Page/Perfumes';
-
-/*const DynamicPerfumes = dynamic(() => import('./Page/Perfumes'), {
-  loading: () => <p>Chargement des parfums...</p>
-});*/
-const DynamicContacts = dynamic(() => import('./Page/Contacts'), {
-  loading: () => <p>Chargement des coordonnées...</p>
-});
+import Contacts from './Page/Contacts';
 
 export default function Home() {
   return (
     <>
       <Accueil />
       <Perfumes />
-      <DynamicContacts />
+      <Contacts />
     </>
   );
 }

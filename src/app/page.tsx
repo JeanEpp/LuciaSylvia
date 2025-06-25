80ddfc879745598c03cfe63f330579e6565ff246
jeanepp <jeanepp99@gmail.com>
2025-06-25 22:01:40 +0200

    Fixing first load

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import dynamic from 'next/dynamic';
 import Accueil from "./Page/Accueil";
+import Perfumes from './Page/Perfumes';
 
-const DynamicPerfumes = dynamic(() => import('./Page/Perfumes'), {
+/*const DynamicPerfumes = dynamic(() => import('./Page/Perfumes'), {
   loading: () => <p>Chargement des parfums...</p>
-});
+});*/
 const DynamicContacts = dynamic(() => import('./Page/Contacts'), {
   loading: () => <p>Chargement des coordonnées...</p>
 });
@@ -12,7 +13,7 @@ export default function Home() {
   return (
     <>
       <Accueil />
-      <DynamicPerfumes />
+      <Perfumes />
       <DynamicContacts />
     </>
   );

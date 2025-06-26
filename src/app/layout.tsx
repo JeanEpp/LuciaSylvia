90aa9041f78e9f52aed6796ecbb7e3e22321cb30
jeanepp <jeanepp99@gmail.com>
2025-06-26 13:56:06 +0200

    adding favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="favicon.ico" />
+      </head>
       <body className={`antialiased top-0`}>
         <Providers>
             <Navbar />

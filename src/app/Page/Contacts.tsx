2ed1acb76a6b539b8f6ccb7edae76af64d7a1563
jeanepp <jeanepp99@gmail.com>
2025-06-26 13:24:59 +0200

    cleaning imported modules and correcting contact display

--- src/app/Page/Contacts.tsx
+++ src/app/Page/Contacts.tsx
@@ -10,7 +10,7 @@ export default function Contacts() {
             <p className={`${font} text-2xl lg:text-3xl max-w-3xl text-center pb-5 sm:pb-10`}>
                 {"Pour toute commande, n'hesitez pas à me contacter par Email, Sms ou Téléphone, avec les parfums que vous souhaiter, leurs formats 5/10/50 ml, ainsi que votre adresse."}
             </p>
-            <div className="mx-auto bg-[#262626] backdrop-blur-sm rounded-3xl max-w-2xl py-12 border-4 border-white">
+            <div className="bg-[#262626] backdrop-blur-sm w-full rounded-3xl max-w-2xl py-12 border-4 border-white">
                 <div className="text-center space-y-8">
                     <h1 className={`text-[32px] font-semibold text-balance text-white sm:text-5xl ${font}`}>Mes coordonnées :</h1>
                     <Link className={`block text-[27px] font-medium text-pretty underline text-white sm:text-4xl ${font}`} href="mailto:parfumerie@luciasylvia.fr" target="_blank">parfumerie@luciasylvia.fr</Link>

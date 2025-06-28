0aa87b44771cab83868a5b0a49e7cb7913da77e8
jeanepp <jeanepp99@gmail.com>
2025-06-28 10:13:26 +0200

    Adding open graph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,25 @@ import Navbar from "./Components/Navbar";
 export const metadata: Metadata = {
   title: "Lucia Sylvia",
   description: "Parfumerie naturelle",
+  icons: {
+    icon: "/favicon.ico",
+  },
+  openGraph: {
+    title: 'Lucia Sylvia',
+    description: 'Découvrez Lucia Sylvia, votre parfumerie naturelle en ligne. Explorez une gamme de parfums artisanaux, créés avec passion et respect de la nature. Commandez dès maintenant pour une expérience olfactive unique.',
+    url: 'https://www.luciasylvia.fr',
+    siteName: 'Lucia Sylvia - Parfumerie Naturelle',
+    images: [
+      {
+        url: 'https://www.luciasylvia.fr/favicon.ico',
+        width: 1200,
+        height: 630,
+        alt: 'Lucia Sylvia - Logo',
+      },
+    ],
+    locale: 'fr_FR',
+    type: 'website',
+  }
 };
 
 export default function RootLayout({
@@ -15,13 +34,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="favicon.ico" />
-      </head>
       <body className={`antialiased top-0`}>
         <Providers>
-            <Navbar />
-            {children}
+          <Navbar />
+          {children}
         </Providers>
       </body>
     </html>

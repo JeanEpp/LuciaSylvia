113c543d92d314c4fe68afba89f14d827ba14e69
jeanepp <jeanepp99@gmail.com>
2025-07-07 20:35:05 +0200

    Test compilation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,9 @@
 import Accueil from "./Page/Accueil";
 import Perfumes from './Page/Perfumes';
 import Contacts from './Page/Contacts';
-import StructuredData from './Components/StructuredData';
 
-import type { WithContext, Organization } from 'schema-dts';
 
-const organizationSchema: WithContext<Organization> = {
-  "@context": "https://schema.org",
-  "@type": "Organization",
-  "name": "Lucia Sylvia - Parfumerie Naturelle",
-  "url": "https://www.luciasylvia.fr",
-  "logo": "https://www.luciasylvia.fr/favicon.ico",
-  "description": "Découvrez Lucia Sylvia, votre parfumerie naturelle en ligne. Parfums artisanaux, éthiques et inspirés par la nature.",
-  "sameAs": [
-  ]
-};
+
 
 export default function Home() {
   return (
@@ -22,7 +11,6 @@ export default function Home() {
       <Accueil />
       <Perfumes />
       <Contacts />
-      <StructuredData data={organizationSchema} />
     </>
   );
 }

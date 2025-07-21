57b3e40faf6e9be5f88d1a813bf4f245ca805279
jeanepp <jeanepp99@gmail.com>
2025-07-21 14:20:34 +0200

    Correcting params type

--- src/app/perfume/[perfumeName]/page.tsx
+++ src/app/perfume/[perfumeName]/page.tsx
@@ -10,14 +10,14 @@ export async function generateStaticParams() {
   }))
 }
 
-export default async function Page({ params }: { params: { perfumeName: string } }) {
-  const paramsLoaded = await params;
+export default async function Page({ params }: { params: Promise<{ perfumeName: string }> }) {
+  const { perfumeName } = await params;
   const perfumes = await getPerfumeNames();
-  const etiquette = perfumes.find((p: IEtiquette) => p.name === paramsLoaded.perfumeName) ?? null;
+  const etiquette = perfumes.find((p: IEtiquette) => p.name === perfumeName) ?? null;
 
   return (
     <div id="Parfums" className="h-min:screen text-center justify-items-center pb-8 pt-20 content-center space-y-9">
-      <h1 className={`text-5xl font-semibold text-balance text-white text-center sm:text-5xl font-carattere`}>{paramsLoaded.perfumeName ?? ""}</h1>
+      <h1 className={`text-5xl font-semibold text-balance text-white text-center sm:text-5xl font-carattere`}>{perfumeName ?? ""}</h1>
       <PerfumeClient etiquette={etiquette} />
       <Contacts/>
     </div>

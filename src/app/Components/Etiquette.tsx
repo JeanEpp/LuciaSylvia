7e7225a8f12063adfd4c235c8b5ba6930f2c3e2a
jeanepp <jeanepp99@gmail.com>
2025-07-28 13:26:52 +0200

    Correcting libellule image

--- src/app/Components/Etiquette.tsx
+++ src/app/Components/Etiquette.tsx
@@ -19,7 +19,7 @@ const Etiquette: React.FC<EtiqettesProps> = ({ onClick, className, text, directi
                 <p className={`rotate-90 text-3xl h-[60%] flex items-center text-white justify-center hover:text-[#e6b94a] transition duration-100 text-nowrap ${animate ? "animate-fade-in" : "animate-fade-out"}`}>
                     {text}
                 </p>
-                <Image className="mt-auto rotate-[15.82deg] pr-1" src={"/Libellule.webp"} alt={"Libellule"} width={37} height={42} />
+                <Image className="mt-auto rotate-[15.82deg] pr-1" src={"/Libellule.webp"} alt={"Libellule"} width={60} height={68} />
                 <div className='relative flex items-center justify-center text-2xl'>
                     <p className="absolute top-[-10px]">LS</p>
                 </div>

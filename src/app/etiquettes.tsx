df64ab4260b8e14ecdc9beec836e1526a75a3c6f
jeanepp <jeanepp99@gmail.com>
2025-08-03 18:20:57 +0200

    Ajout d'une interface etiquette pour le json

--- src/app/etiquettes.tsx
+++ src/app/etiquettes.tsx
@@ -20,8 +20,16 @@ export interface IEtiquette {
     image: string[];
 }
 
+interface IRawEtiquette {
+    name: string;
+    description: string;
+    composition: string[];
+    price: number;
+    availability: string;
+}
+
 export default async function getPerfumeNames(): Promise<IEtiquette[]> {
-  const etiquettes: IEtiquette[] = Data.Etiquettes.map((etiquette: any) => {
+  const etiquettes: IEtiquette[] = Data.Etiquettes.map((etiquette: IRawEtiquette) => {
     const url = etiquette.name.replace(/ /g, "-").replace(/é/g, "e");
     return {
       ...etiquette,
